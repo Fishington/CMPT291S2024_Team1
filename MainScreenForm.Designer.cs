c05051f25b4d248f56482fbc7ed1a12f59db4e32
Aidan <fishera34@mymacewan.ca>
2024-06-02 13:21:33 -0600

    Created figma forms, and added navigation. Might need to look into some sort of system for creating new branches.

--- MainScreenForm.Designer.cs
+++ MainScreenForm.Designer.cs
@@ -28,34 +28,89 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.LogOff_BTN_MainScreen = new System.Windows.Forms.Button();
+            this.button4 = new System.Windows.Forms.Button();
+            this.button1 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.button3 = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
             this.SuspendLayout();
             // 
-            // LogOff_BTN_MainScreen
+            // button4
             // 
-            this.LogOff_BTN_MainScreen.BackColor = System.Drawing.Color.IndianRed;
-            this.LogOff_BTN_MainScreen.Location = new System.Drawing.Point(12, 502);
-            this.LogOff_BTN_MainScreen.Name = "LogOff_BTN_MainScreen";
-            this.LogOff_BTN_MainScreen.Size = new System.Drawing.Size(95, 41);
-            this.LogOff_BTN_MainScreen.TabIndex = 4;
-            this.LogOff_BTN_MainScreen.Text = "Log Off";
-            this.LogOff_BTN_MainScreen.UseVisualStyleBackColor = false;
-            this.LogOff_BTN_MainScreen.Click += new System.EventHandler(this.button4_Click);
+            this.button4.ForeColor = System.Drawing.SystemColors.ControlText;
+            this.button4.Location = new System.Drawing.Point(765, 501);
+            this.button4.Name = "button4";
+            this.button4.Size = new System.Drawing.Size(234, 139);
+            this.button4.TabIndex = 5;
+            this.button4.Text = "Exit";
+            this.button4.UseVisualStyleBackColor = true;
+            this.button4.Click += new System.EventHandler(this.button4_Click_1);
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(514, 501);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(234, 139);
+            this.button1.TabIndex = 6;
+            this.button1.Text = "Reports";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(263, 501);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(234, 139);
+            this.button2.TabIndex = 7;
+            this.button2.Text = "New Rental";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // button3
+            // 
+            this.button3.Location = new System.Drawing.Point(12, 501);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(234, 139);
+            this.button3.TabIndex = 8;
+            this.button3.Text = "Inventory";
+            this.button3.UseVisualStyleBackColor = true;
+            this.button3.Click += new System.EventHandler(this.button3_Click);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Inter SemiBold", 18F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(43, 251);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(529, 51);
+            this.label1.TabIndex = 9;
+            this.label1.Text = "Welcome to Car Program";
             // 
             // MainScreenForm
             // 
-            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleDimensions = new System.Drawing.SizeF(11F, 24F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(954, 555);
+            this.ClientSize = new System.Drawing.Size(1013, 652);
             this.ControlBox = false;
-            this.Controls.Add(this.LogOff_BTN_MainScreen);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.button3);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.button4);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedToolWindow;
+            this.Margin = new System.Windows.Forms.Padding(6);
             this.Name = "MainScreenForm";
             this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
             this.ResumeLayout(false);
+            this.PerformLayout();
 
         }
 
         #endregion
-        private System.Windows.Forms.Button LogOff_BTN_MainScreen;
+        private System.Windows.Forms.Button button4;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Button button3;
+        private System.Windows.Forms.Label label1;
     }
 }
