8939c448f77717c8235d0e89b9ed45467e116bb4
Bob B <bobblinde@gmail.com>
2024-06-03 16:53:14 -0600

    Don't manually specify Branch ID

--- dummydata.sql
+++ dummydata.sql
@@ -2,6 +2,6 @@ USE CarRental
 insert into CarType (Type, DailyPrice, WeeklyPrice, MonthlyPrice) VALUES ('SMALL', 5, 15, 10)
 insert into CarType (Type, DailyPrice, WeeklyPrice, MonthlyPrice) VALUES ('SUV', 5, 15, 10)
 
-insert into Branches (Branch_ID, Name, Street, City, Postal_Code, Province, Country) VALUES (1, 'Edmonton South', 'Calgary Trail', 'T5Z ZZZ', 'AB', 'Canada')
+insert into Branches (Name, Street, City, Postal_Code, Province, Country) VALUES  ('Edmonton South', 'Calgary Trail', 'Edmonton', 'T5Z ZZZ', 'AB', 'Canada')
 
-insert into Cars(VIN, License_Plate, Make, Model, Transmission, Branch_ID, Type) VALUES ('WA1DGAFP4EA042058', 'BobRox', 'Volkswagen', 'Golf', 'Manual', 1)
+insert into Cars(VIN, License_Plate, Make, Model, Transmission, Branch_ID, Type) VALUES ('WA1DGAFP4EA042058', 'BobRox', 'Volkswagen', 'Golf', 'Manual', 1, 'SMALL')
