9d47f4d99ab9d331750abc6e405bff8d422cdaff
Trevor Farquhar <trevorfarquhar2003@gmail.com>
2024-06-06 15:39:14 -0600

    connects to database and/or creates CarRental DB

--- Program.cs
+++ Program.cs
@@ -3,20 +3,38 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using System.Data.SqlClient;
 
 namespace Team1CMPT291_Final
 {
     internal static class Program
     {
+        const string serverConnectionString = "Server=localhost;Integrated Security=true;";
+        const string databaseConnectionString = "Server=localhost;Database=CarRental;Integrated Security=true;";
+        const string databaseName = "CarRental";
         /// <summary>
         /// The main entry point for the application.
         /// </summary>
         [STAThread]
         static void Main()
         {
+            EnsureDatabaseExists();
             Application.EnableVisualStyles();
             Application.SetCompatibleTextRenderingDefault(false);
             Application.Run(new MainScreenForm());
+        }
+
+        static void EnsureDatabaseExists()
+        {
+            using (SqlConnection connection = new SqlConnection(serverConnectionString))
+            {
+                connection.Open();
+                using (SqlCommand command = new SqlCommand(
+                    $"IF DB_ID('{databaseName}') IS NULL CREATE DATABASE [{databaseName}]", connection))
+                {
+                    command.ExecuteNonQuery();
+                }
+            }
         }
     }
 }
