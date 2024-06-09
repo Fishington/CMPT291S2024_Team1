3343d2ed5f453a5d3e2d0d82df24c17ea4ed6a6b
Aidan <fishera34@mymacewan.ca>
2024-06-09 10:35:47 -0600

    Created new Modify Car From to modify cars. Have not done Logic to fill in textboxes or save button. VIN gets passed through but that is it at the moment.

---
+++ ModifyCarForm.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Team1CMPT291_Final
+{
+    public partial class ModifyCarForm : Form
+    { 
+
+        public ModifyCarForm(string selectedVIN)
+        {
+            InitializeComponent();
+
+            textbox_VIN.Text = selectedVIN;
+
+
+        }
+
+        private void BackBTN_Click(object sender, EventArgs e)
+        {
+            this.Close();
+        }
+    }
+}
